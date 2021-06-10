45f742f447baf3b5c2288daab63a0398e4699c7b
Julia Vavrinyuk <juliav@Julias-MacBook-Pro.local>
2021-06-09 20:10:15 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ServiceRequest from "./components/ServiceRequest";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MY-SDK</h1>
+      </header>
+      <ServiceRequest />
+    </div>
+  );
+}
+
+export default App;
