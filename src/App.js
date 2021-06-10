d222b581d53ddc038afbda1fe30c82735d9ad4e3
Julia Vavrinyuk <juliav@Julias-MacBook-Pro.local>
2021-06-09 20:27:22 -0500

    added service request form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
-import ServiceRequest from "./components/ServiceRequest";
+import ServiceRequest from "./components/ServiceRequest/ServiceRequest";
 
 
 function App() {
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('You have submitted the form.')
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>MY-SDK</h1>
       </header>
       <ServiceRequest />
+      <form onSubmit={handleSubmit}>
+      <fieldset>
+         <label>
+           <p>Employee Name</p>
+           <input name="name" />
+           <p>Request Type</p>
+           <input RequestType="RequestType" />
+           <p>Description</p>
+           <input Description="Description" />
+         </label>
+       </fieldset>
+       <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
