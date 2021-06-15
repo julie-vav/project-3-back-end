5563910ef67e2f90e448105822eceeaa4714a68f
Julia Vavrinyuk <juliav@Julias-MacBook-Pro.local>
2021-06-15 12:10:13 -0400

    commit all

--- project-3-back-end/routes/requests.js
+++ project-3-back-end/routes/requests.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var Request = require('../models/request');
 
 router.get('/', function(req, res, next) {
-  res.status(200).json([
-    {name: 'julia.vavrinyuk', request: 'VPN', description: 'System not connecting'}
-  ]);
+  Request.find({}, function(err, requests) {
+    res.status(200).json(requests);
+  });
 });
 
 router.post('/', function(req, res) {
